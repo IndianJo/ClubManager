06327e7debc48a668c87fcb68576593fe265c2f2
Joseph <joseph.mesny@gmail.com>
2015-08-11 17:43:20 +0200

    Ajout entité player pour gérer les joueurs ajout d'un loader pour générer un jeu de test de joueurs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,10 +14,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FB\ClubManagerBundle\FBClubManagerBundle(),
-            new Propel\PropelBundle\PropelBundle(),
-            );
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+        );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
---
+++ src/FB/ClubManagerBundle/DataFixtures/ORM/LoadPlayer.php
@@ -0,0 +1,43 @@
+<?php
+// src\FB\ClubManagerBundle\DataFixtures\ORM\LoadPlayer.php
+/**
+ * Created by PhpStorm.
+ * User: Joseph
+ * Date: 11/08/2015
+ * Time: 16:36
+ */
+
+namespace FB\ClubManagerBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use FB\ClubManagerBundle\Entity\Player;
+
+class LoadPlayer implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $Players = array(array());
+        $Players[0][0] = "joseph";
+        $Players[0][1] = "mesny";
+
+        $Players[1][0] = "tommy";
+        $Players[1][1] = "quirico";
+
+        $Players[2][0] = "clementine";
+        $Players[2][1] = "gamonet";
+
+        $Players[3][0] = "colin";
+        $Players[3][1] = "michoudet";
+
+
+        foreach ($Players as $currentPlayer){
+            $player = new player();
+            $player->setFirstname($currentPlayer[0]);
+            $player->setLastname($currentPlayer[1]);
+
+            $manager->persist($player);
+        }
+        $manager->flush();
+    }
+}
