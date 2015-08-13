c97f99fcf5043943e6da7c5f8b2ecbc040e7ae25
Joseph <joseph.mesny@gmail.com>
2015-08-13 14:13:36 +0200

    add link to form bundle Ajout d'un lien vers la partie forum le lien n'est pas encore fonctionnel, mise en place d'une redirection jusqu'a mise en place du bundle forum definitif

--- src/FB/ClubManagerBundle/Controller/ManagerController.php
+++ src/FB/ClubManagerBundle/Controller/ManagerController.php
@@ -11,6 +11,7 @@ namespace FB\ClubManagerBundle\Controller;
 
 use Symfony\Component\BrowserKit\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 class ManagerController extends Controller
@@ -21,5 +22,12 @@ class ManagerController extends Controller
     public function indexAction()
     {
        return $this->render('FBClubManagerBundle:ClubManager:index.html.twig');
+    }
+
+    public function forumAction()
+    {
+        $this->get('session')->getFlashBag()->add('info', 'Le forum n\' est pas encore intégré');
+        
+        return $this->redirect($this->generateUrl('fb_clubmanager_home'));
     }
 }
