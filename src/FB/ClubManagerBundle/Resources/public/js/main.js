9bbdcf8bdb0ab8e275a66678671c77e69332da71
Joseph <joseph.mesny@gmail.com>
2015-09-15 09:28:46 +0200

    update java scritp include file When I include adesigns calendar jquery 2.1.1 min.js the collapse function use in tournament list (to display player) does't work.

--- src/FB/ClubManagerBundle/Resources/public/js/main.js
+++ src/FB/ClubManagerBundle/Resources/public/js/main.js
@@ -8,7 +8,7 @@ $('body').on('click', '[data-toggle=collapse-next]', function (e) {
 
     // Try to close all of the collapse areas first
     var parent_id = $(this).data('parent');
-    $(parent_id+' .panel-collapse').collapse('hide');
+    $(parent_id+'.panel-collapse').collapse('hide');
 
     // ...then open just the one we want
     var $target = $(this).parents('.panel').find('.panel-collapse');
